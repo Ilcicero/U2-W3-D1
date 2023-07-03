f941b82661e3c478a778638af44c269c4a17c5d0
Vincenzo Cicero <cicero.vinz@gmail.com>
2023-07-03 13:43:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(_firstName, _lastName, _age, _location) {
+      // uso il metodo constructor quando devo assegnare dei valori dinamici a determinate proprietà
+      this.firstName = _firstName
+      this.lastName = _lastName
+      this.age = _age
+      this.location = _location
+    }
+}
